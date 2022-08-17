9bbcabed7ce152837f0fead3171eea0866d4b92c
Юрий <mulch@list.ru>
2022-08-17 17:17:34 +0300

    изменен файл конфигурации добавлено удаление ряда типов сообщений

---
+++ AntiSpamBot/config.py
@@ -0,0 +1,8 @@
+# конфигурация бота (константы)
+token = '5430864257:AAHsIGljFlHg_BpYoW7o9AKqi4Q-hw6tNRc'
+
+black_sibvols = set('!"№;%:?*()_+-~!@#$^&\/<>{}[]|1234567890.,=')
+user = ''
+mat = []
+bot_settings = {'forbidden_message': ('audio', 'photo', 'sticker', 'video', 'video_note', 'voice')}
+
