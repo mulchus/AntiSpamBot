6c05e1306d97e0d81acbf6b0aa425e233de87691
mulchus <mulch@list.ru>
2022-08-22 23:25:01 +0300

    настроил удаление кнопки инлайн клавиатуры

--- AntiSpamBot/config.py
+++ AntiSpamBot/config.py
@@ -6,4 +6,11 @@ user = ''
 mat = []
 bot_settings = {'forbidden_message': ('audio', 'photo', 'sticker', 'video', 'video_note', 'voice')}
 pause = 3
+
+HELP = 'Бот супебот имеет следующие команды:' \
+       '/addword слово - добавить слово в базу мата' \
+       '/delword слово - удлить слово из базы мата' \
+       '/show admins - вывести список админов чата' \
+       '/pause число - изменить паузу отображения замечания'
+
 #
