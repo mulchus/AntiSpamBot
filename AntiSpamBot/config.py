f6f34e2ba99c8fc6672d90eeb5f28df434b293e7
mulchus <mulch@list.ru>
2022-09-05 23:03:08 +0300

    сделал конфиг-файл для разных чатов теперь надо добавить фильтр GIF и смайлов - какой это тип сообщений?

--- AntiSpamBot/config.py
+++ AntiSpamBot/config.py
@@ -4,10 +4,10 @@ token = '5430864257:AAHsIGljFlHg_BpYoW7o9AKqi4Q-hw6tNRc'
 black_simvols = set('!"№;%:?*()_+-~!@#$^&\/<>{}[]|1234567890.,=')
 user = ''
 mat = []
-all_types_message = ['audio', 'photo', 'sticker', 'video', 'video_note', 'voice']
-bot_settings = {}
+all_types_message = ['audio', 'photo', 'sticker', 'video', 'video_note', 'voice', 'animation']
+bot_settings = []
 # bot_settings['forbidden_message'] = all_types_message # не идет - изменяется и переменная и словарь одновременно
-bot_settings['forbidden_message'] = ['audio', 'photo', 'sticker', 'video', 'video_note', 'voice']
+#bot_settings['forbidden_message'] = ['audio', 'photo', 'sticker', 'video', 'video_note', 'voice']
 pause = 3
 
 help = 'Бот супебот имеет следующие команды:' \
