05953631c5a257785bf77dee83790839b4ef2cda
Юрий <mulch@list.ru>
2022-09-07 17:11:45 +0300

    поиск и внедрение новых типов сообщений - еще не реализовано

--- AntiSpamBot/config.py
+++ AntiSpamBot/config.py
@@ -4,16 +4,14 @@ token = '5430864257:AAHsIGljFlHg_BpYoW7o9AKqi4Q-hw6tNRc'
 black_simvols = set('!"№;%:?*()_+-~!@#$^&\/<>{}[]|1234567890.,=')
 user = ''
 mat = []
-all_types_message = ['audio', 'photo', 'sticker', 'video', 'video_note', 'voice', 'animation']
+all_types_message = ['audio', 'photo', 'sticker', 'video', 'video_note', 'voice', 'document', 'sticker']
 bot_settings = []
 # bot_settings['forbidden_message'] = all_types_message # не идет - изменяется и переменная и словарь одновременно
 #bot_settings['forbidden_message'] = ['audio', 'photo', 'sticker', 'video', 'video_note', 'voice']
 pause = 3
 
-help = 'Бот супебот имеет следующие команды:' \
-       '/addword слово - добавить слово в базу мата' \
-       '/delword слово - удлить слово из базы мата' \
-       '/show admins - вывести список админов чата' \
-       '/pause число - изменить паузу отображения замечания'
-
-#
+help = '''Бот супербот имеет следующие команды: \n
+       /addword слово - добавить слово в базу мата \n
+       /delword слово - удлить слово из базы мата \n
+       /show admins - вывести список админов чата \n
+       /pause число - изменить паузу отображения замечания'''
