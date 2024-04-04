0d00b98d63eb2f12adf17bb13ccd45922d359629
mulchus <mulch@list.ru>
2024-04-04 14:35:46 +0300

    update bot_settings, add logger, add kick_chat_member

--- antispambot.py
+++ antispambot.py
@@ -3,6 +3,9 @@ import time
 import markups as m
 import json
 import config
+import logging
+import logging.handlers
+import sys
 
 from environs import Env
 # from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
@@ -10,41 +13,55 @@ from telebot.apihelper import ApiTelegramException
 from telebot.util import update_types
 from datetime import datetime
 
-
 previous_markup = None
 previous_message = None
-# info_message = None
 time_start = ''
 time_end = ''
 
+LOGFILE = 'logs.txt'
+logger = logging.getLogger()
 env = Env()
 env.read_env()
 bot_token = env('BOT_TOKEN')
-
 bot = telebot.TeleBot(bot_token)
+
 
 # кнопка СТАРТ
 # button_start = KeyboardButton('СТАРТ!')
 # greet_kb = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True).add(KeyboardButton('СТАРТ!'))
 
 
+def configuring_logging():
+    logger.setLevel(logging.INFO)
+    logger_handler = logging.StreamHandler()
+    # logger_handler = logging.handlers.RotatingFileHandler(
+    #     LOGFILE, maxBytes=(1048576*5), backupCount=3
+    # )
+    logger_formatter = logging.Formatter(
+        '%(asctime)s : %(levelname)s : %(message)s',
+        datefmt='%d-%m-%Y %H:%M:%S'
+    )
+    logger_handler.setFormatter(logger_formatter)
+    logger.addHandler(logger_handler)
+    return logger
+
+
 # запуск бота для инициализации стартовых процессов:
 @bot.message_handler(commands=['start'])
 def start(message):
+    logger.info('СТАРТУЮ!!!')
     bot.send_message(message.chat.id, f"Меню в чате № {message.chat.id}!", reply_markup=m.start_markup)
     with open('bot_settings.json', 'r+') as file:
         config.bot_settings = json.load(file)
-        print(config.bot_settings)
-        print(config.bot_settings.keys())
-    
-    # TODO: Есть ошибка проверки на наличие чата в списке конфига, дублирует с ID = int
-    if message.chat.id in config.bot_settings.keys():
-        print('Я знаю этот чат!')
+        logger.info(config.bot_settings)
+        logger.info(config.bot_settings.keys())
+    if str(message.chat.id) in config.bot_settings.keys():
+        logger.info('Я знаю этот чат!')
     else:   # если знакомый чат не найден - добавляем текущий чат в список конфига
-        chat_settings = {'forbidden_messages': config.other_types_of_message, 'controlled_users': []}
-        config.bot_settings[message.chat.id] = chat_settings
+        chat_settings = {'forbidden_messages': config.other_types_of_message, 'controlled_users': {}}
+        config.bot_settings[str(message.chat.id)] = chat_settings
         bot_settings_save(config.bot_settings)
-        print(config.bot_settings)
+        logger.info(config.bot_settings)
     if not config.mat:  # если словарь мата еще пустой
         with open('wordfilter.txt') as file:  # создание списка плохих слов из файла txt
             config.mat = file.read().split(', ')
@@ -192,7 +209,7 @@ def addword(message):
             info_message = bot.send_message(message.chat.id, f'В базе плохих слов есть слово "{newword}"')
             del_bot_mes(message.chat.id, message.message_id, info_message.message_id)
     except Exception as e:
-        print(repr(e))
+        logger.error(repr(e))
     if config.command_from_markup:
         config.command_from_markup = None
 
@@ -213,7 +230,7 @@ def delword(message):
             info_message = bot.send_message(message.chat.id, f'В базе плохих слов нет слова "{word}"')
             del_bot_mes(message.chat.id, message.message_id, info_message.message_id)
     except Exception as e:
-        print(repr(e))
+        logger.error(repr(e))
 
 
 # вывод сообщений, возможных к обработке
@@ -275,7 +292,7 @@ def help_(message):
 def delete(message):
     # print(message)
     _, user, time_1, time_2 = message.text.split(maxsplit=3)
-    print(datetime.fromtimestamp(message.date + 10800).strftime("%H:%M"))
+    logger.info(datetime.fromtimestamp(message.date + 10800).strftime("%H:%M"))
     try:
         time_start_ = datetime.combine(datetime.now().date(), datetime.strptime(time_1, "%H-%M").time())
     except ValueError:
@@ -284,11 +301,11 @@ def delete(message):
     else:
         time_start_sec = int((time_start_ - datetime(1970, 1, 1)).total_seconds() - 10800)
         # print(time_start_)
-        print(time_start_sec)
+        logger.info(time_start_sec)
         time_end_ = datetime.combine(datetime.now().date(), datetime.strptime(time_2, "%H-%M").time())
         time_end_sec = int((time_end_ - datetime(1970, 1, 1)).total_seconds() - 10800)
         # print(time_end_)
-        print(time_end_sec)
+        logger.info(time_end_sec)
 
         # for i in range(time_end_sec, time_start_sec, -1):
 
@@ -296,10 +313,10 @@ def delete(message):
         for _ in message.id:  # Пройдёмся по всем entities в поисках ссылок
             # url - обычная ссылка, text_link - ссылка, скрытая под текстом
             if message.from_user.username == user and message.date in range(time_start_sec, time_end_sec):
-                print(message.date)
+                logger.info(message.date)
                 # Мы можем не проверять chat.id, он проверяется ещё в хэндлере
                 bot.delete_message(message.chat.id, message.message_id)
-                print(f'Удалено сообщение от {message.from_user.username}, {message.chat.id}, {message.id}')
+                logger.info(f'Удалено сообщение от {message.from_user.username}, {message.chat.id}, {message.id}')
             else:
                 return
 
@@ -307,18 +324,17 @@ def delete(message):
 # проверка поступившего сообщения на плохие слова
 @bot.message_handler(content_types=['text'])
 def bad_text(message):
-    print(message.from_user.id)
-    print(message.text)
-    print(config.financial_words)
-    print(len(set(message.text.split(' ')).intersection(config.financial_words)))
+    logger.info(message.from_user.id)
+    logger.info(message.text)
+
+
     message_words = set([word.lower() for word in message.text.split(' ')])
-    if (message.from_user.id in config.bot_settings[message.chat.id].get('controlled_users') and
+    if (str(message.from_user.id) in config.bot_settings[str(message.chat.id)]['controlled_users'].keys() and
             len(message_words.intersection(config.financial_words)) > 0):
-        print(f'Впойман криптоман {message.from_user.username}, удаляем!!!')
+        logger.info(f'Впойман криптоман {message.from_user.username}, удаляем!!!')
         info_message = bot.send_message(message.chat.id, f'Впойман криптоман {message.from_user.username}, удаляем!!!')
         del_bot_mes(message.chat.id, message.message_id, info_message.message_id)
-        
-        # TODO: Добавить блокировку и удаление юзера.
+        bot.kick_chat_member(message.chat.id, message.from_user.id)
         return
     try:
         mats_words = []
@@ -330,55 +346,39 @@ def bad_text(message):
             info_message = bot.send_message(message.chat.id, f'Зачем ругаешься? Ты сказал "{mats_words}"!')
             del_bot_mes(message.chat.id, message.message_id, info_message.message_id)
     except Exception as e:
-        print(repr(e))
+        logger.error(repr(e))
 
 
 # проверка поступившего сообщения на графику, стикеры, видео, аудио и удаление его, а также позже - удаление
 # сообщения бота
 @bot.message_handler(content_types=config.other_types_of_message)
 def bad_message(message):
-    print(message.chat.id)
-    print(message.content_type)
+    logger.info(message.chat.id)
+    logger.info(message.content_type)
     try:
         # for chat_settings in config.bot_settings:
         #     if message.chat.id in chat_settings.values():
-        if message.content_type in config.bot_settings[message.chat.id]['forbidden_messages']:
+        if message.content_type in config.bot_settings[str(message.chat.id)]['forbidden_messages']:
             info_message = bot.send_message(message.chat.id, f'Размещение формата \
                                                     \b{message.content_type}\b запрещено! Удаляю!')
             del_bot_mes(message.chat.id, message.message_id, info_message.message_id)
     except Exception as e:
-        print(repr(e))
+        logger.error(repr(e))
     
     
 @bot.chat_member_handler()
 def check_member_login(updated):
-    # print('chat_member_handler -----> ', updated)
-    if updated.new_chat_member:
-        # print(updated.new_chat_member)
-        # print(updated.new_chat_member.status)
-        # print(updated.new_chat_member.user.id)
-        # print(updated.new_chat_member.user.username)
-        # print(updated.new_chat_member.user.first_name)
-        # print(updated.new_chat_member.user.last_name)
-        # print(updated.new_chat_member.user.language_code)
-        # print(updated.new_chat_member.user.is_bot)
-
-        # try:
-        # print(updated.chat.id)
-        # print(config.bot_settings.get(updated.chat.id))
-        # print(config.bot_settings)
-        # print(config.bot_settings[updated.chat.id].get('controlled_users'))
-        
-        # controlled_users = config.bot_settings[updated.chat.id].get('controlled_users')
-        # print('Я знаю юзеров: ', controlled_users)
-        if updated.new_chat_member.user.id in config.bot_settings[updated.chat.id].get('controlled_users'):
-            print('Я знаю этого юзера!')
-        else:
-            print('Я НЕ знаю этого юзера! Добавил')
-            config.bot_settings[updated.chat.id]['controlled_users'].append(updated.new_chat_member.user.id)
-            bot_settings_save(config.bot_settings)
-        # except Exception as e:
-        #     print(repr(e))
+    try:
+        if updated.new_chat_member:
+            if str(updated.new_chat_member.user.id) in config.bot_settings[str(updated.chat.id)]['controlled_users'].keys():
+                logger.info('Я знаю этого юзера!')
+            else:
+                logger.info('Я НЕ знаю этого юзера! Добавил.')
+                config.bot_settings[str(updated.chat.id)]['controlled_users'][str(updated.new_chat_member.user.id)] = \
+                    {'addition time': datetime.now().strftime("%d-%m-%Y %H:%M:%S")}
+                bot_settings_save(config.bot_settings)
+    except Exception as e:
+        logger.error(repr(e))
         
 
 # @bot.message_handler(func=lambda message: message.entities is not None and message.chat.id == message.chat.id)
@@ -391,18 +391,14 @@ def check_member_login(updated):
 #         else:
 #             return
 
-# time_start = datetime.fromtimestamp(time_1).strftime("%H:%M")
-# time_end = datetime.fromtimestamp(time_2).strftime("%H:%M")
+
+def main():
+    logger = configuring_logging()
+    logger.info('ЗАПУСТИЛСЯ')
+    #TODO переместить сюда или под IF __name__ переменные окружения и инстал бота
+    bot.infinity_polling(allowed_updates=update_types)
 
 
-# print(msg)
-# time.sleep(2)
-# bot.edit_message_text(chat_id = message.chat.id, message_id = msg.message_id, text = \
-# f'ой, ты написал "{message.text}"')
-# tb.edit_message_text(new_text, chat_id, message_id)
-# photo = open('/tmp/photo.png', 'rb')
-# tb.send_photo(chat_id, photo)
-# tb.send_photo(chat_id, "FILEID")
+if __name__ == "__main__":
+    main()
 
-bot.infinity_polling(allowed_updates=update_types)
-# bot.polling(none_stop=True)
